b5d4bee2891154a62f33443e3920c019f791d384
Andreas Bigger <andreas.bigger@coinbase.com>
2026-02-01 11:38:09 -0500

    chore(dkg,runner): add unused_crate_dependencies lint and remove dead deps

--- crates/node/runner/src/lib.rs
+++ crates/node/runner/src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![doc(issue_tracker_base_url = "https://github.com/refcell/kora/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 // Trait signatures require impl Future return type
 #![allow(clippy::manual_async_fn)]
 
